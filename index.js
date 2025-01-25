323ab9db24b2275a20cf4b4f7856e5299e578c6a
Faizan-Ansari <ansarizada70@gmail.com>
2025-01-25 05:07:38 -0800

    Update index.js
    chnage the PORT

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/authRoutes');
 const crudRoute = require('./routes/crudRoutes');
 require('dotenv').config();
-
-
 const App = express();
-App.use(express.json())
-App.use('/', authRoute)
-App.use('/', crudRoute)
+const cors = require('cors');
 
+// Middleware
+App.use(cors());
+App.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-    .then((res) => {
-        App.listen(process.env.PORT || 5000, () => {
-            console.log('Db Connected and Server started');
+// Routes
+App.use('/', authRoute);
+App.use('/', crudRoute);
+
+// Database connection and server setup
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const PORT = process.env.PORT || 5000; // Use dynamic port for Vercel
+        App.listen(PORT, () => {
+            console.log(`Db Connected and Server started on PORT ${PORT}`);
         });
     })
     .catch((err) => {
-        console.log(err, 'error')
-    })
+        console.log('Error connecting to DB:', err.message);
+    });
