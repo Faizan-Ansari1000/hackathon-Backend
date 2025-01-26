16318cf2197e96098dcc246efe763b286a9d5301
Faizan-Ansari <ansarizada70@gmail.com>
2025-01-25 16:13:22 -0800

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,11 @@
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+    name: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin","user"],default: 'user' }, 
 });
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
